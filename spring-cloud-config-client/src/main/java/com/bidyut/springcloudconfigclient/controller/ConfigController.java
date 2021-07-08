772fac4c09f917c2f2214fa39a98d82b3a6a56f4
Bidyut Kalita <bidyut.kalita@wibmodc.com>
2021-07-08 11:14:04 +0530

    config alient added

---
+++ spring-cloud-config-client/src/main/java/com/bidyut/springcloudconfigclient/controller/ConfigController.java
@@ -0,0 +1,21 @@
+package com.bidyut.springcloudconfigclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+	
+	
+	@Value("${greetings.message}")
+	String greetings;
+	@GetMapping("/test")
+	public String getCloudConfig()
+	{
+		return greetings;
+	}
+
+}
